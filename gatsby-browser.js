40e7384ce4def08c252a992e63d6231a02557669
Markos Konstantopoulos <markoskonstanto@hotmail.com>
2020-05-07 12:05:33 +0300

    Initial

---
+++ gatsby-browser.js
@@ -0,0 +1,3 @@
+import "@affectionatedoor/opentype"
+import "./src/styles/work-sans-1.css"
+import "./src/styles/main.css"
---
+++ src/pages/languages.jsx
@@ -0,0 +1,321 @@
+import React from "react"
+
+export default () => (
+  <main style={{ fontSize: "17px" }}>
+    <h1 style={{ marginBottom: "calc(var(--rhythm) * 2)" }}>
+      Work Sans language support
+    </h1>
+    <p>
+      The text is from the{" "}
+      <a href="http://weiweihuanghuang.github.io/Work-Sans/">project’s site.</a>
+    </p>
+    <section className="language">
+      <h2>Italian</h2>
+      <h3
+        style={{
+          textAlign: "center",
+          lineHeight: 1.2,
+          marginBottom: "var(--rhythm)",
+        }}
+      >
+        Antonio Fogazzaro
+        <br />
+        Il Dolore nell’Arte
+        <br />
+        DISCORSO
+        <br />
+        MILANO
+        <br />
+        CASA EDITRICE BALDINI, CASTOLDI & C.º
+        <br />
+        ‘Galleria Vittorio Emanuele, 17 e 80’
+        <br />
+        1901
+        <br />
+        PROPRIETÀ LETTERARIA
+        <br />
+        MILANO—TIP. PIROLA & CELLA DI P. CELLA
+      </h3>
+      <div style={{ textAlign: "center" }}>
+        NOTA.—Il presente discorso, tenuto la prima volta in
+        <br />
+        Torino l’11 aprile 1900 per invito di quella Società di
+        <br />
+        Cultura, venne quindi pubblicato nella ‘Rassegna Nazionale’.
+      </div>
+      <p>
+        <em style={{ fontStyle: "normal", fontWeight: 700 }}>I</em>
+        <br />
+        Sull’orlo di un lago bizzarro che io amo, verde ai due capi, sottile e
+        torto per sinuose gole di colli selvaggi e di montagne tragiche, sereno
+        a mezzo il corso nell’arco di un golfo idilliaco, si affaccia allo
+        specchio maggiore delle acque una densa e signorile corona di ombra.
+        Sovente per le vie solitarie di quell’ombra fui preso dal senso di una
+        bellezza che più si prometta di quanto si sveli. Non la scoprivo intera
+        nel tremolar lucente del lago tra i tronchi, nelle pensose montagne
+        assise a levante del bosco, nelle alte scene lontane, dorate di sole,
+        che mi apparivano tratto tratto a settentrione. Mi sorgeva invece nel
+        cuore e me lo riempiva di sè l’idea di una possibile parola unica nella
+        quale consuonassero tante diverse voci di cose; di una profonda parola
+        di bellezza, tentante e inafferrabile come la parola di accordi musicali
+        che annuncino, preparino una successiva rivelazione di suoni e invece si
+        spengano senza seguito nel silenzio. Così penetrato dall’anima occulta
+        delle cose che mi figuravo desiderosa e incapace di esprimersi a me
+        com’ero io di comprender lei, movevo alla più recondita sede di quel
+        regno di ombra dove i maggiori alberi, fronteggiandosi in giro,
+        congiungendosi a grande altezza in un’ascensione unica, fanno di sè
+        ghirlanda e tempio a un cupo fantasma.
+      </p>
+      <p>
+        Una giovine donna, bellissima, dai capelli scomposti, dalle vesti
+        cadenti, siede là sopra un alto seggio, piegato il busto gentile in
+        avanti, puntati i gomiti alle ginocchia, strette le guance fra i pugni
+        chiusi, fissi gli occhi tordidi nel vuoto. Il viso rivela una
+        intelligenza forte che affonda nella follia. Nessuna cura stringe più
+        costei nè del mondo nè di sè. Nessun vivente presuma, per esserle stato
+        caro, poterle recar conforto. Ella non torcerebbe un momento gli occhi
+        suoi avidi dalla visione di angoscia che la impietra; e tuttavia ci
+        balena che possa repente balzar dal seggio con uno strido, avventarsi là
+        dove guarda, tanto potente vita spirò nel marmo il grande artista che le
+        pose nome «Desolazione». Si soffre davanti all’alta Dolorosa, e si gode
+        intensamente di soffrire. Ci partiamo pensosi e la visione di lei ne
+        persegue al sole, per le ombre che il vento scompiglia, lungo le rive
+        sonore del lago scintillante. Non ci guasta l’incanto dei colori e dei
+        suoni ma vi spira una malinconia segreta che lo rende più soave, infonde
+        alle voci delle cose un accento nuovo e profondo. Pare che l’enigma di
+        bellezza oscura onde avemmo dianzi turbato il cuore vi ritorni, lo prema
+        più forte, quasi vi si disveli. I susurri del fogliame paiono prima dire
+        dire incalzando e poi dolersi, nel venir meno, di non essere intesi.
+      </p>
+    </section>
+    <Separator />
+    <section className="language">
+      <h2>French</h2>
+      <h3>Étienne François Dralet « L’art du taupier »</h3>
+      <p>
+        La zoologie range la Taupe dans la classe des mammifères, dans l’ordre
+        des carnassiers, dans la famille des insectivores, dans la tribu des
+        Talpidés, où elle constitue le genre Talpa, placé entre ceux Desman
+        (Myogale) et Condylure (Condylura).
+      </p>
+      <p>
+        Jusqu’à présent, on connaît trois espèces dans le genre Taupe: la Taupe
+        Woogura, la Taupe aveugle et la Taupe d’Europe, ou commune.
+      </p>
+      <p>
+        La Taupe Woogura (Talpa Woogura), récemment découverte au Japon, ne
+        diffère de celle commune que par son pelage de couleur fauve sale et en
+        ce qu’elle ne possède que trois paires d’incisives à chaque mâchoire,
+        tandis que les deux autres espèces en ont quatre à la mâchoire
+        inférieure; ses mœurs sont identiques.
+      </p>
+      <p>
+        « La Taupe aveugle (Talpa cæca) est ainsi nommée, parce que son œil est
+        recouvert par une membrane mince, translucide, percée en avant de la
+        pupille d’un trou très-fin, non dilatable, par lequel on peut voir le
+        globe de l’organe. Quant aux autres points de l’organisation, la Taupe
+        aveugle se distingue peu de la Taupe vulgaire; elle aurait cependant la
+        trompe plus longue, les incisives supérieures plus larges, les lèvres,
+        les pieds et la queue blancs au lieu d’être gris. Son pelage épais et
+        velouté est gris-noir foncé, la pointe des poils étant d’un noir brun;
+        sa taille est la même que celle de la Taupe commune. » (A. E. Brehm,
+        l’Homme et les animaux, t. Ier, p. 756–757.) [Illustration: Fig. 1.—La
+        Taupe commune.]
+      </p>
+      <p>
+        La Taupe commune ou d’Europe (Talpa Europæa) est un petit mammifère
+        fouisseur, à corps long et cylindrique, à pattes très-courtes, à tête
+        prolongée en avant en forme de groin ou de boutoir, avec des yeux si
+        petits et si bien cachés sous les poils qu’on a longtemps nié leur
+        existence, dépourvue de conque de l’oreille externe, munie enfin d’un
+        simple rudiment de queue. Son corps est recouvert d’un poil fin, serré,
+        court, mou, imitant le velours, de couleur noire avec des reflets
+        grisâtres et rougeâtres; la longueur totale, du bout du nez à
+        l’extrémité de la queue, est de om, 15 à om, 16 chez les adultes.
+      </p>
+      <p>
+        « La Taupe commune se trouve dans toute l’Europe, à quelques exceptions
+        près, et arrive jusque dans l’Asie centrale et septentrionale. Beaucoup
+        de naturalistes ne voient dans la Taupe américaine qu’une variété de
+        notre espèce. En Europe, le midi de la France, la Lombardie et le nord
+        de l’Italie dessinent sa limite méridionale. De là, elle remonte vers le
+        nord jusqu’à Dovrefjeld; en Grande-Bretagne, jusque vers l’Écosse
+        centrale; en Russie, jusqu’au milieu du bassin de la Dwina. Elle manque
+        complétement dans les Orcades, les Shetlands, la plus grande partie des
+        Hébrides et en Islande. En Asie, elle va du Caucase jusqu’à la Léna.
+        Dans les Alpes, elle monte jusqu’à une altitude de 2,000 mètres. Partout
+        elle est commune et se multiplie d’une manière surprenante, là où elle
+        n’a pas d’ennemis. » (Brehm, ut supra, p. 747.)
+      </p>
+    </section>
+    <Separator />
+    <section className="language">
+      <h2>Spanish</h2>
+      <h3>Gabriel García Márquez «Cien años de soledad»</h3>
+      <p>
+        Muchos años después, frente al pelotón de fusilamiento, el coronel
+        Aureliano Buendía había de recordar aquella tarde remota en que su padre
+        lo llevó a conocer el hielo. Macondo era entonces una aldea de veinte
+        casas de barro y cañabrava construidas a la orilla de un río de aguas
+        diáfanas que se precipitaban por un lecho de piedras pulidas, blancas y
+        enormes como huevos prehistóricos. El mundo era tan reciente, que muchas
+        cosas carecían de nombre, y para mencionarlas había que señalarías con
+        el dedo. Todos los años, por el mes de marzo, una familia de gitanos
+        desarrapados plantaba su carpa cerca de la aldea, y con un grande
+        alboroto de pitos y timbales daban a conocer los nuevos inventos.
+        Primero llevaron el imán. Un gitano corpulento, de barba montaraz y
+        manos de gorrión, que se presentó con el nombre de Melquiades, hizo una
+        truculenta demostración pública de lo que él mismo llamaba la octava
+        maravilla de los sabios alquimistas de Macedonia. Fue de casa en casa
+        arrastrando dos lingotes metálicos, y todo el mundo se espantó al ver
+        que los calderos, las pailas, las tenazas y los anafes se caían de su
+        sitio, y las maderas crujían por la desesperación de los clavos y los
+        tornillos tratando de desenclavarse, y aun los objetos perdidos desde
+        hacía mucho tiempo aparecían por donde más se les había buscado, y se
+        arrastraban en desbandada turbulenta detrás de los fierros mágicos de
+        Melquíades. «Las cosas, tienen vida propia pregonaba el gitano con
+        áspero acento, todo es cuestión de despertarles el ánima.» José Arcadio
+        Buendía, cuya desaforada imaginación iba siempre más lejos que el
+        ingenio de la naturaleza, y aun más allá del milagro y la magia, pensó
+        que era posible servirse de aquella invención inútil para desentrañar el
+        oro de la tierra. Melquíades, que era un hombre honrado, le previno:
+        «Para eso no sirve.» Pero José Arcadio Buendía no creía en aquel tiempo
+        en la honradez de los gitanos, así que cambió su mulo y una partida de
+        chivos por los dos lingotes imantados. Úrsula Iguarán, su mujer, que
+        contaba con aquellos animales para ensanchar el desmedrado patrimonio
+        doméstico, no consiguió disuadirlo. «Muy pronto ha de sobrarnos oro para
+        empedrar la casa», replicó su marido. Durante varios meses se empeñó en
+        demostrar el acierto de sus conjeturas. Exploró palmo a palmo la región,
+        inclusive el fondo del río, arrastrando los dos lingotes de hierro y
+        recitando en voz alta el conjuro de Melquíades. Lo único que logró
+        desenterrar fue una armadura del siglo XV con todas sus partes soldadas
+        por un cascote de óxido, cuyo interior tenía la resonancia hueca de un
+        enorme calabazo lleno de piedras. Cuando José Arcadio Buendía y los
+        cuatro hombres de su expedición lograron desarticular la armadura,
+        encontraron dentro un esqueleto calcificado que llevaba colgado en el
+        cuello un relicario de cobre con un rizo de mujer.
+      </p>
+    </section>
+    <Separator />
+    <section className="language">
+      <h2>Swedish</h2>
+      <h3>August Strindberg „Röda rummet”</h3>
+      <p>
+        Och det är höst igen; det är en klar novembermorgon då Arvid Falk beger
+        sig från sin numera eleganta bostad på Storgatan till den manska
+        flickpensionen vid Karl XIII:s torg, där han skall göra sitt inträde som
+        lärare i Svenska Språket och Historien. Han har väl begagnat
+        höstmånaderna för att göra sitt återinträde i det civiliserade samhället
+        och har därvid erfarit vilken barbar han under sina strövtåg blivit; han
+        har avlagt sin rövarhatt och skaffat sig en ny, hög, vilken till en
+        början hade svårt att sitta kvar; han har köpt sig handskar, men var så
+        förvildad, att när mamselln i boden frågade hans nummer, svarade han 15,
+        vilket framkallade en hel hop små skratt från de många mamsellerna
+        därinne; modet har undergått stora förändringar, sedan han sist köpte
+        sig kläder och han tycker sig som en söndagssprätt, när han går fram på
+        gatorna, och han speglar sig då och då i bodfönsterna för att se efter
+        om allting sitter rätt. Nu går han av och an på trottoaren utanför
+        Dramatiska Teatern och väntar att klockan i Jakob skall slå nio; han
+        känner sig orolig och besvärad, alldeles som när han själv skulle gå i
+        skolan; trottoaren är för kort, och han tycker sig löpa som en hund i en
+        kedja, när han vänder och vänder på sina spår; ett ögonblick tänker han
+        allvarsamt på att sträcka ut promenaden, långt ut, ty han vet att man
+        kommer till Lill-Jans om man fortsätter gatan framåt och han erinrar sig
+        den morgonen då samma trottoar ledde honom ut från det samhälle han
+        flydde till friheten, naturen och – slaveriet!
+      </p>
+      <p>
+        Klockan blir nio! Han står i tamburen; dörrarne till skolsalen äro
+        stängda; i skymningen ser han så många små barnplagg hänga runtomkring
+        väggarne; hattar, boas, kråkor, baschliks, vantar, muffar ligga på
+        borden och i fönsternischerna och på golven stå hela regementen med
+        knäppkängor och galoscher. Men det luktar icke fuktiga kläder och vått
+        läder som i tamburen på riksdagen, eller arbetarförbundet Nordstjärnan,
+        eller – ah, det gick en doft som av nyslaget hö förbi – den kom bestämt
+        från den där lilla muffen, som är vit som en kattunge med svarta noppor
+        och blått sidenfoder med hängande tofsar; han kunde icke underlåta att
+        ta den i sin hand och låta parfymen new-mown hay – men farstudörren
+        öppnas, och in kommer en liten tioåring åtföljd av sin jungfru; hon ser
+        på magistern med ett par stora orädda ögon och gör en liten kokett
+        nigning, vilken magistern nästan förlägen besvarar med en bugning,
+        varvid den lilla skönheten ler – och jungfrun också! Hon kommer för
+        sent, men det tyckes icke skrämma henne, ty hon låter jungfrun ta av sig
+        ytterkläder och bottiner med en min så lugn som om hon kommit till en
+        bal. Nu – vilka ljud inifrån rummen – det knäppte i hans bröst – vad var
+        det? – Ah – Det var ju orgeln! Hm! Gamla orgeln! Ja!
+      </p>
+      <Separator />
+      <section className="language">
+        <h2>Turkish</h2>
+        <h3>Orhan Pamuk “Cevdet Bey ve Oğulları”</h3>
+        <p>
+          “Geceliğin kolu da, sırtım da… Bütün sınıf da… Çarşaflar da… Of, of,
+          of, bütün yatak da sırılsıklam! Evet, her şey sırılsıklam ve ben
+          uyandım!” diye mırıldandı Cevdet Bey. Her şey az önce gördüğü rüyadaki
+          gibi sırılsıklamdı. Yatağında homurdanarak döndü, rüyayı hatırladı ve
+          korktu. Rüyada, Kula’daki sübyan mektebinde hocanın karşısında
+          oturuyordu. Başını ıslak yastıktan kaldırıp doğruldu. “Evet, hocanın
+          karşısında oturuyorduk. Bütün okul diz boyu suya gömülmüştü,” diye
+          söylendi. “Niye gö­ mülmüştü?” Çünkü okulun tavanı akıyordu. Tavandan
+          akan tuzlu sular benim alnımdan ve göğsümden dökülüyor, bütün odaya
+          yayılıyordu. Hoca da değneği ile bütün sınıfa beni gös­ teriyor, “Hep
+          bu Cevdet’in yüzünden,” diyordu. Hocanın değ- neğiyle kendisini nasıl
+          gösterdiğini, bütün arkadaşlarının dönüp suçlayarak ve küçümseyerek
+          kendisine nasıl baktıklarını, kendisinden iki yaş büyük ağbisinin de
+          herkesten çok kendisini küçümsediğini gözünün önünde canlandırınca
+          ürperdi. Ama gözünü kırpmadan bütün sınıfı bir solukta falakadan
+          geçiren, bir tokatla bir oğlanı bayıltan hoca, bir türlü gelip
+          tavandan akan sular için kendisini cezalandırmıyordu. Cevdet Bey,
+          “Herkesten başkaydım, yalnızdım, beni küçümsüyorlardı,” diye düşündü.
+        </p>
+        <p>
+          {" "}
+          “Ama hiçbiri gelip bana dokunmaya cesaret edemiyor, sular da bütün
+          okulu döktürüyordu!” Korkunç rüya bir anda neşeli ve hoş bir anı
+          oluverdi: “Ben başkaydım, yalnızdım, ama beni cezalandıramıyorlardı.”
+          Bir kere okulun damına çıkıp, kiremitleri kırdığını hatırlayarak ayağa
+          kalktı. “Kiremitleri kırmıştım. Kaç yaşındaydım? Yedi yaşındaydım.
+          Şimdi otuzyediyim, nişanlandım, yakında evleneceğim.” Nişanlısını
+          hatırlayınca heyecanlandı. “Evet, yakında evleneceğim, sonra… Aman
+          hâlâ oyalanıyorum! Geç kaldım!” Vakti anlamak için önce pencereye
+          koştu, perdelerin arasından dışarıya baktı. Dışarıda tuhaf bir ışık ve
+          sis vardı. Güneşin doğduğunu anladı. Sonra bu eski alışkanlığına
+          kızarak dönüp, saatine baktı: Alaturka yarım. “Aman, aman geç kal­
+          mayayım!” diye söylenerek helaya koştu.
+        </p>
+        <p>
+          Yıkanıp temizlenince neşesi daha da arttı. Tıraş olurken gene rüyayı
+          düşündü. Sonra Şükrü Paşa’nın konağına gideceğini hatırlayıp, o yeni
+          ve temiz pantolonla ceketi, yakası kolalı kartonlaşmış gömleği ve
+          zarif bulduğu kravatı taktı. Başına nişan töreninden önce kalıplattığı
+          fesini oturttu. Küçük masa aynasında kendini seyretti ve istediği gibi
+          olduğuna karar verdi, ama gene de içinde bir hüzün uyandı. Bütün bu
+          şık kıyafette, nişanlısının konağına gideceği için telâşlanmasında
+          gülünç birşeyler ol­ malıydı. Bu küçük ve zararsız hüzünle perdeleri
+          açtı. Sis Şeh- zadebaşı Camii’nin minarelerini örtmüş, ama kubbeyi
+          gizleye­ memişti. Yan bahçedeki çardak her zamankinden daha yeşildi.
+          “Sıcak bir gün olacak!” diye düşündü. Çardağın altında bir kedi ağır
+          ağır yalanıyordu. Cevdet Bey bir şey hatırlayarak pencereden uzandı ve
+          gördü: Kupa arabası da gelmiş, evin önünde durmuştu. Atlar
+          kuyruklarını sallıyorlar, Cevdet Bey’i bekleyen arabacı kapının önünde
+          sigara içiyordu. Cevdet Bey hatırladığı sigara paketini ve çakmağını,
+          cüzdanını ve bir kere daha baktığı saatini ceplerine yerleştirerek
+          odadan çıktı.
+        </p>
+      </section>
+      <Separator />
+    </section>
+  </main>
+)
+
+const Separator = () => (
+  <div
+    style={{
+      textAlign: "center",
+      padding: "var(--rhythm)",
+      marginTop: "calc(var(--rhythm) * 2)",
+    }}
+  >
+    * * *
+  </div>
+)
